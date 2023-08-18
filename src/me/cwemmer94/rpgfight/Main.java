0ddd7c0ee2ed6556209d96d250a36a44819d8768
Michael Smith <msmith@codecellgames.com>
2023-08-17 20:25:55 -0400

    Update Main.java

--- src/me/cwemmer94/rpgfight/Main.java
+++ src/me/cwemmer94/rpgfight/Main.java
@@ -6,6 +6,12 @@ public class Main extends EmberEngine
 {
 	public static void main(String[] args)
 	{
-		
+		new Main().setup();
+	}
+
+	@Override
+	public void setup() 
+	{
+		System.out.println("Hello, World!");
 	}
 }
